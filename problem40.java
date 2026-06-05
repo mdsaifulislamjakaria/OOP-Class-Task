4b678a06379cd93e0f395d73e661195f94029aee
mdsaifulislamjakaria <amdsijakaria@gmail.com>
2026-06-05 17:00:10 +0600

    Added problem 40 solution

---
+++ problem40.java
@@ -0,0 +1,15 @@
+// problem 4(a);
+import java.util.*;
+
+public class problem40 {
+    public static void main(String[] args){
+        try(Scanner sc=new Scanner(System.in)){
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(j +" ");
+                } System.out.println("");
+            }
+        }
+    }
+}
