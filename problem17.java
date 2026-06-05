088f1d97db06d8e1ffda5a38bb7d6ac0edd1a198
mdsaifulislamjakaria <amdsijakaria@gmail.com>
2026-06-05 16:47:39 +0600

    Added problem 17 solution

---
+++ problem17.java
@@ -0,0 +1,12 @@
+// problem 1(h);
+import java.util.*;
+
+public class problem17 {
+    public static void main(String[] args){
+        try(Scanner sc=new Scanner(System.in)){
+            int a=sc.nextInt();
+            if(a%2==0) System.out.println("Even");
+            else System.out.println("Odd");
+        }
+    }
+}
