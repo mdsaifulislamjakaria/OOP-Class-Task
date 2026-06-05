35f91d930e2d5aa28ba8ce015b655a6b9818d8f0
mdsaifulislamjakaria <amdsijakaria@gmail.com>
2026-06-05 16:47:06 +0600

    Added problem 16 solution

---
+++ problem16.java
@@ -0,0 +1,12 @@
+// problem 1(g);
+import java.util.*;
+
+public class problem16 {
+    public static void main(String[] args){
+        try(Scanner sc=new Scanner(System.in)){
+            int a=sc.nextInt();
+            if(a>=0) System.out.println("Positive");
+            else System.out.println("negative");
+        }
+    }
+}
